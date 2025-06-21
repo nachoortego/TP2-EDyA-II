9e53a01f68efb62db6cb6c6d534bc8d336da0486
Nacho Ortego <79536480+nachoortego@users.noreply.github.com>
2025-06-20 22:28:25 -0300

    ArrSeq.hs nuevas funciones implementadas

--- src/ArrSeq.hs
+++ src/ArrSeq.hs
@@ -9,4 +9,19 @@ instance Seq [] where
 
     singletonS x = A.fromList [x]
 
-    
+    lengthS = length
+
+    nthS s i = s ! i
+
+    tabulateS = A.tabulate
+
+    mapS f s = A.tabulate (\i -> f (nth i s)) (lengthS s)
+
+    filterS p s = let x = nthS 1 s
+                      (px, fxs) = p x ||| filterS p xs
+                  in case px of
+                      True -> x : fxs -- ver como appendear
+                      False -> fxs
+
+    appendS s t = let (ls, lt) = lengthS s ||| lengthS t
+                  in  A.tabulate (\i -> if i < ls then (nthS i s) else (nthS (i - ls) t)) (ls + lt)
