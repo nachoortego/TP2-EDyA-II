6e9686890600173f5dc9870bcb7186f64fb21858
Nacho Ortego <79536480+nachoortego@users.noreply.github.com>
2025-06-25 19:42:30 -0300

    Update ArrSeq.hs

--- src/ArrSeq.hs
+++ src/ArrSeq.hs
@@ -9,19 +9,25 @@ instance Seq [] where
 
     singletonS x = A.fromList [x]
 
-    lengthS = length
+    lengthS = A.length
 
     nthS s i = s ! i
 
     tabulateS = A.tabulate
 
-    mapS f s = A.tabulate (\i -> f (nth i s)) (lengthS s)
+    mapS f s = A.tabulate (\i -> f (nthS i s)) (lengthS s)
 
-    filterS p s = let x = nthS 1 s
-                      (px, fxs) = p x ||| filterS p xs
-                  in case px of
-                      True -> x : fxs -- ver como appendear
-                      False -> fxs
+    filterS p s | length s == 1 = let x = nthS s 0
+                                  in case p x of
+                                       True -> x
+                                       False -> empty
+                | otherwise = let h = div (length s) 2
+                                  (fl, fr) = filterS (take h s) ||| filterS (drop h s)
+                                  in append fl fr 
 
     appendS s t = let (ls, lt) = lengthS s ||| lengthS t
                   in  A.tabulate (\i -> if i < ls then (nthS i s) else (nthS (i - ls) t)) (ls + lt)
+
+    takeS s n = A.subArray 0 n s
+
+    dropS s n = A.subArray (lengthS s - n) (lengthS s) s
